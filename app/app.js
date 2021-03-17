aedded00deacece859389859c045203be3e66215
Brian Beeler <brianlbeeler@gmail.com>
2021-03-17 11:41:23 -0500

    sudo code of beginning workflow.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,35 @@
-getLocationByZip(42721, (data)=>{
+const { regexp } = require("sequelize/types/lib/operators");
+const { getWeatherByPoint } = require("../controllers/external.controller");
+
+let zipcode = prompt('Choose a 5 digit zip code to search the weather.')
+let zipPattern = new regexp('^\d{5}$')
+let validZip = zipPattern.test(zipcode);
+
+if (validZip) {
+    getLocationByZip(zip, (data)=>{
+        // Success flow
+        
+        // disaplay location data
+        // ask if correct
+        // render check whether and try again buttons
+
+    }, () => {
+        // render: there was an error
+        // render: try again button
+
+    })
+
+} else {
+    // render improper zipcode message with try again button
+}
+
+
+let onCheckWeather() => {
+    getWeatherByPoint()
+}
+
+
+getLocationByZip(zip, (data) {
     //Success
     console.log("Success", data.lat, data.lng);
 
