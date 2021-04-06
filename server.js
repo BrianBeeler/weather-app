d2ce369fde08e4c7ad4537e89935a2d38583067a
Brian Beeler <brianlbeeler@gmail.com>
2021-04-06 15:49:56 -0500

    Connected to smaller database.

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use(express.static(path.join(__dirname, 'react-app/build')));
 const db = require("./models");
 
 // Refreshes database each time the server runs (dev mode)
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
 
 // simple route
 app.get('/', (req,res) => {
