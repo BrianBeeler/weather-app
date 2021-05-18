a897022c077672d2fbad6b9be8decc60d68889c7
Brian Beeler <brianlbeeler@gmail.com>
2021-05-18 10:39:59 -0500

    Changed to run on secure port.

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('*', function(req, res){
 // TODO: Set up static 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 443;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
